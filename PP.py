c9cd024732b8a15975f7fd69fe8b176797d4d52e
andrisleduskrasts <aleduskrasts@gmail.com>
2015-05-26 22:45:20 +0300

    Update PP.py

--- PP.py
+++ PP.py
@@ -40,7 +40,7 @@ def textsplitter(content):
 				sentences[counter-1] = sentences[counter-1]+sentences[counter+1]
 				sentences.pop(counter+1)
 				sentences.pop(counter)
-		elif re.match('[a-zāčēģīķļņšūž].*|[.]', sentence):
+		elif re.match('[a-zāčēģīķļņšūž].*|[.?!]', sentence):
 			if counter!=0:
 				sentences[counter-1] = sentences[counter-1] + sentence
 				sentences.pop(counter)
@@ -72,7 +72,7 @@ def grammarCheck(sentences):
 				wordlist = nee(wordlist, counter)
 			if re.match('Iespējams$', word):
 				wordlist = iespejams(wordlist, counter)
-			if re.match('[Gg]an[,.?!]?$', word):
+			if re.match('gan[,.?!]?$', word):
 				wordlist = gan(wordlist, counter)
 			if re.match('kur[,.?!]?$', word):
 				wordlist = kur(wordlist, counter)
@@ -80,7 +80,24 @@ def grammarCheck(sentences):
 				wordlist = lai(wordlist, counter)
 			if re.match('jo[,.?!]?$|jo[.][.][.]', word):
 				wordlist = jo(wordlist, counter)
-
+			if re.match('kad[,.?!]?$|kad[.][.][.]', word):
+				wordlist = kad(wordlist, counter)
+			if re.match('tostarp[,.?]?$|tostarp[.][.][.]', word):
+				wordlist = tostarp(wordlist, counter)
+			if re.match('[Šš]ķiet$', word):
+				wordlist = skiet(wordlist, counter)
+			if re.match('ne[,.?!]?$|ne[.][.][.]', word):
+				wordlist = ne(wordlist, counter)
+			if re.match('kaut$', word):
+				if len(wordlist)>counter:
+					if re.match('[Gg]an[,.:;!?]?$', wordlist[counter+1]):
+						wordlist = kautgan(wordlist, counter)
+					elif re.match('[Aa]rī[,.:;!?]?$', wordlist[counter+1]):
+						wordlist = kautari(wordlist, counter)
+			if re.match('kāpēc[,.:;!?]?$', word):
+				wordlist = kapec(wordlist, counter)
+			if re.match('cik[,.:?!?]?$', word):
+				wordlist = cik(wordlist, counter)
 
 		#result is changed back into sentence for later return
 		sentences[sentencecounter] = " ".join(word for word in wordlist)
@@ -172,7 +189,7 @@ def protams(wordlist, number):
 #"jā" function
 def jaa(wordlist, number):
 	import re
-	if re.match('.*[(,;:-]|[""]?[nN]u,?$|[""]?[uU]n$|[""]?[vV]ai$|[""]?[kK]a$|[""]?[jJ]o$|[""]?[bB]et$|[""]?[vV]arbūt$|[""]?[gG]an$|[""]?[tT]ad$|[""]?[kK]ur$|[Kk]uram[,.]?$|[Kk]uriem[,.]?$|[Kk]uru[,.]?$|[Kk]urām[,.]?$|[Kk]urā[,.]?$', wordlist[number-1]) or number == 0:
+	if re.match('.*[(,;:-]|[""]?[nN]u,?$|[aA]rī$|[""]?[uU]n$|[""]?[vV]ai$|[""]?[kK]a$|[""]?[jJ]o$|[""]?[bB]et$|[""]?[vV]arbūt$|[""]?[gG]an$|[""]?[tT]ad$|[""]?[kK]ur$|[Kk]uram[,.]?$|[Kk]uriem[,.]?$|[Kk]uru[,.]?$|[Kk]urām[,.]?$|[Kk]urā[,.]?$|[kK]ad[,.?]$', wordlist[number-1]) or number == 0:
 		if re.match('nu$', wordlist[number-1]):
 			if re.match('nu,$', wordlist[number-1]):
 				wordlist[number-1] = wordlist[number-1]
@@ -230,7 +247,7 @@ def jaa(wordlist, number):
 #"nē" function
 def nee(wordlist, number):
 	import re
-	if re.match('.*[(,;:-]|[""]?[nN]u,?$|[""]?[uU]n$|[""]?[vV]ai$|[""]?[kK]a$|[""]?[jJ]o$|[""]?[bB]et$|[""]?[vV]arbūt$|[""]?[gG]an$|[""]?[tT]ad$|[""]?[kK]ur$|[Kk]uram[,.]?$|[Kk]uriem[,.]?$|[Kk]uru[,.]?$|[Kk]urām[,.]?$|[Kk]urā[,.]?$', wordlist[number-1]) or number == 0:
+	if re.match('.*[(,;:-]|[""]?[nN]u,?$|[""]?[uU]n$|[""]?[vV]ai$|[""]?[kK]a$|[""]?[jJ]o$|[""]?[bB]et$|[""]?[vV]arbūt$|[""]?[gG]an$|[""]?[tT]ad$|[""]?[kK]ur$|[Kk]uram[,.]?$|[Kk]uriem[,.]?$|[Kk]uru[,.]?$|[Kk]urām[,.]?$|[Kk]urā[,.]?$|[kK]ad[,.]?$|[aA]rī$', wordlist[number-1]) or number == 0:
 		if re.match('nu$', wordlist[number-1]):
 			if re.match('nu,$', wordlist[number-1]):
 				wordlist[number-1] = wordlist[number-1]
@@ -267,8 +284,11 @@ def nee(wordlist, number):
 def iespejams(wordlist, number):
 	import re
 	if number == 0:
-		if re.match('.*t[,.!?:;]?$|.*ties[,.!?:;]?$|-$', wordlist[number+1]) or re.match('.*t[,.!?:;]?$|.*ties[,.!?:;]?$', wordlist[number+2]):
+		if re.match('.*t[,.!?:;]?$|.*ties[,.!?:;]?$|-$', wordlist[number+1]):
 			return wordlist
+		elif len(wordlist)>2:
+			if re.match('.*t[,.!?:;]?$|.*ties[,.!?:;]?$', wordlist[number+2]):
+				return wordlist
 		else:
 			wordlist[number] = wordlist[number] + ','
 			return wordlist
@@ -279,24 +299,29 @@ def gan(wordlist, number):
 	import re
 	if number == 0:
 		return wordlist
-	counter = number-4
-	if counter < 0:
-		counter = 0
-	if re.match('.*[,-]$', wordlist[number-1]):
+	counter = 0
+	temp = 0
+	if re.match('.*[,:-]$|[kK]aut$', wordlist[number-1]):
 		return wordlist
 	else:
 		while counter < number-1:
-			if re.match('[gG]an[,]?', wordlist[counter]):
-				wordlist[number-1] = wordlist[number-1] + ','
-				return wordlist
+			if re.match('[gG]an[,]?$', wordlist[counter]):
+				if counter > 0:
+					if re.match('[kK]aut$', wordlist[counter-1]):
+						temp = counter
+				if temp < 1:
+					wordlist[number-1] = wordlist[number-1] + ','
+					return wordlist
 			counter = counter+1
-	return wordlist
+		return wordlist
 #"kur" function
 def kur(wordlist, number):
 	import re
 	if number == 0:
 		return wordlist
 	if re.match('.*[,;:-]$', wordlist[number-1]):
+		return wordlist
+	if re.match('[kK][Aa][Uu][Tt]$|[dD][iI][eE][zZ]$', wordlist[number-1]):
 		return wordlist
 	else:
 		counter = 0
@@ -317,32 +342,41 @@ def lai(wordlist, number):
 	#regex method import
 	import re
 	#checking of punctuation in the previous word
+	if len(wordlist) < number:
+		return wordlist
 	if re.match('.*[,;:-]', wordlist[number-1]):
 		return wordlist
 	else:
 		counter = 0
-		testword = 0
+		testword1 = 0
+		testword2 = 0
 		#"lai arī", "un" and "lai" interaction 
+		if len(wordlist) == number:
+			return wordlist
 		while counter < number:
 			if re.match('[Ll]ai[,-]?$', wordlist[counter]):
 				temp = counter +1
 				if re.match('[Aa]rī[,:-]?$', wordlist[temp]):
-					testword = temp
+					testword1 = temp
 					counter = counter+1
+				elif re.match('[gG]an[,:-]?$', wordlist[temp]):
+					testword2 = temp
 				else:
 					while temp < number:
 						if re.match('un[,-]*?$|vai[,-]?$|bet[,-]?$', wordlist[temp]):
 							return wordlist
 						temp = temp+1
 			counter = counter+1
-		if re.match('[aA]rī[,:-]?$', wordlist[number+1]) and testword > 0:
+		if re.match('[aA]rī[,:-]?$', wordlist[number+1]) and testword1 > 0:
+			return wordlist
+		if re.match('[gG]an[,:-]?$', wordlist[number+1]) and testword2 > 0:
 			return wordlist
 		else:
 			#The added exception with question words, can only happen if the position of "lai" is not at the start
 			if number > 0:
-				if re.match('[kK]āpēc|[kK]o$|[kK]ā$|[kK]ur$|[kK]am$', wordlist[number-1]):
+				if re.match('[kK]āpēc|[kK]o$|[kK]ā$|[kK]ur$|[kK]am$|[kK]ad$', wordlist[number-1]):
 					return wordlist
-				elif re.match('[kK]āpēc|[kK]o$|[kK]ā$|[kK]ur$|[kK]am$', wordlist[number-2]):
+				elif re.match('[kK]āpēc|[kK]o$|[kK]ā$|[kK]ur$|[kK]am$|[kK]ad$', wordlist[number-2]):
 					return wordlist
 			#no exceptions have been met, add the punctuation mark and return the sentence
 			wordlist[number-1] = wordlist[number-1] + ','
@@ -353,17 +387,117 @@ def jo(wordlist, number):
 	if number == 0 or re.match('.*[(,;:-]', wordlist[number-1]):
 		return wordlist
 	else:
-		if re.match('[uU]n$', wordlist[number-1]):
-			return wordlist
-		elif re.match('[vV]ēl$', wordlist[number-1]) and re.match('.*āk[s]?[,.!?:;-]?', wordlist[number+1]):
-			return wordlist
+		if len(wordlist)>number:
+			if re.match('[uU]n$', wordlist[number-1]):
+				return wordlist
+			elif re.match('[vV]ēl$', wordlist[number-1]) and re.match('.*āk[s]?[,.!?:;-]?', wordlist[number+1]):
+				return wordlist
+			else:
+				wordlist[number-1] = wordlist[number-1] + ','
+				return wordlist
 		else:
-			wordlist[number-1] = wordlist[number-1] + ','
 			return wordlist
-
+#"kad" function
+def kad(wordlist, number):
+	import re
+	if number == 0:
+		return wordlist
+	if re.match('.*[,;:-]$', wordlist[number-1]):
+		return wordlist
+	if re.match('[kK][Aa][Uu][Tt]$', wordlist[number-1]):
+		return wordlist
+	else:
+		counter = 0
+		while counter < number:
+			if re.match('[Kk]ad[,-]?$|[kK]āpēc[,]?$|[kK]o$[,]?$|[kK]ā$[,]?$|[kK]ur$[,]?$|[kK]am$[,]?$', wordlist[counter]):
+				temp = counter +1
+				while temp < number:
+					if re.match('un[,-]?$|vai[,-]?$|bet[,-]?$', wordlist[temp]):
+						return wordlist
+					temp = temp+1
+			counter = counter+1
+		if number == 1 and re.match('Un|Bet', wordlist[number-1]):
+			return wordlist
+		wordlist[number-1] = wordlist[number-1] + ','
+		return wordlist
+#"tostarp" function
+def tostarp(wordlist, number):
+	import re
+	if number == 0 or re.match('.*[(,;:-]|[Uu]n[,-]?$|[Vv]ai[,-]?$|[Bb]et[,-]?$', wordlist[number-1]):
+		return wordlist
+	else:
+		wordlist[number-1] = wordlist[number-1] + ','
+		return wordlist
+#"šķiet" function
+def skiet(wordlist, number):
+	import re
+	if number == 0:
+		wordlist[number] = wordlist[number] + ','
+		return wordlist
+	else:
+		return wordlist
+#"ne" function
+def ne(wordlist, number):
+	import re
+	if number == 0:
+		return wordlist
+	counter = 0
+	if re.match('.*[,:-]$', wordlist[number-1]):
+		return wordlist
+	else:
+		while counter < number:
+			if re.match('[nN]e[,-]?$', wordlist[counter]):
+				wordlist[number-1] = wordlist[number-1] + ','
+				return wordlist
+			counter = counter+1
+	return wordlist
+#"kaut gan" function
+def kautgan(wordlist, number):
+	import re
+	if number == 0:
+		return wordlist
+	if re.match('.*[,:-]$', wordlist[number-1]):
+		return wordlist
+	else:
+		wordlist[number-1] = wordlist[number-1] + ','
+		return wordlist
+#"kaut arī" function
+def kautari(wordlist, number):
+	import re
+	if number == 0:
+		return wordlist
+	if re.match('.*[,:-]$', wordlist[number-1]):
+		return wordlist
+	if re.match('[uU]n$', wordlist[number-1]) and number == 1:
+		return wordlist
+	else:
+		wordlist[number-1] = wordlist[number-1] + ','
+		return wordlist
+#"kāpēc" function
+def kapec(wordlist, number):
+	import re
+	if number == 0:
+		return wordlist
+	if re.match('.*[,;:-]$|[Uu]n$|[Bb]et$|[Tt]ad$|[Vv]ai$|[Nn]ezin$', wordlist[number-1]):
+		return wordlist
+	else:
+		wordlist[number-1] = wordlist[number-1] + ','
+		return wordlist
+#"cik" function
+def cik(wordlist, number):
+	import re
+	if number == 0:
+		return wordlist
+	if re.match('.*[,;:-]$|[Uu]n$|[Bb]et$|[Tt]ad$|[Vv]ai$|[Nn]ezin$|[Jj]o$|[Pp]ar$|[Uu]z$|[Zz]em$|[Vv]irs$|[Pp]ie$|[Nn]ezin$', wordlist[number-1]):
+		return wordlist
+	if number > 1:
+		if re.match('arī$', wordlist[number-1]) and re.match('[lL]ai$', wordlist[number-2]):
+			return wordlist
+	wordlist[number-1] = wordlist[number-1] + ','
+	return wordlist
 #Interface module
 #specify the input file
-inFile = open('testi/testlai.txt', 'r')
+inFile = open('testi/testkautari.txt', 'r')
 data = loadtext(inFile)
 inFile.close()
 #Split data into sentences using the sentences textsplitter() function
